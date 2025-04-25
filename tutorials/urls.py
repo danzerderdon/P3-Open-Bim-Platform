59cb2385dd93cbfe54dfb4ae7a184f57a9c6ca03
danzerderdon <daniel.danzer.dd@gmail.com>
2025-04-25 20:10:40 +0200

    Filter functin 80%

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import tutorial_create_landing,create_tutorial, TutorialListView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -26,4 +32,6 @@ urlpatterns = [
 
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

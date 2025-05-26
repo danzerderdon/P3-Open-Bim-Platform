f5034995d22ffec9e049eef91ce78eacf9437b1d
danzerderdon <daniel.danzer.dd@gmail.com>
2025-05-26 11:29:34 +0200

    Added Download button

---
+++ tutorials/templatetags/filename_filters.py
@@ -0,0 +1,9 @@
+# tutorials/templatetags/filename_filters.py
+import os
+from django import template
+
+register = template.Library()
+
+@register.filter
+def basename(value):
+    return os.path.basename(value)

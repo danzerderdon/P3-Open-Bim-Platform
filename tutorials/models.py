423720b3a2fb33cf59e6338905cfcb9e7d4edc33
danzerderdon <daniel.danzer.dd@gmail.com>
2025-06-13 16:09:11 +0200

    deleted dead Programversion model

--- tutorials/models.py
+++ tutorials/models.py
@@ -3,13 +3,6 @@ from django.contrib.auth.models import User
 
 
 
-class ProgramVersion(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Tutorial(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()

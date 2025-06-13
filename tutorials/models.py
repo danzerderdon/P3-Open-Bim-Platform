09eeaae82923378e278dc1c756d9644a8b325c6e
danzerderdon <daniel.danzer.dd@gmail.com>
2025-06-13 16:09:35 +0200

    guess is was needed

--- tutorials/models.py
+++ tutorials/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 
 
 
+class ProgramVersion(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Tutorial(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
